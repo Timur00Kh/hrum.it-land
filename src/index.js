d1f9d1815b8f1abb5a0d79d082390e84ec29a2c9
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2022-02-05 14:11:49 +0300

    fix viewport

--- src/index.js
+++ src/index.js
@@ -36,3 +36,10 @@ function sleep(time) {
         showCursor: false,
     })
 })()
+
+let vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
